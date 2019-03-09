9cde449418fa5aa27bc4dcb55dac966866323240
Prashant <faith.prashant@gmail.com>
2019-03-09 23:54:19 +0530

    setinterval set

--- script/main.js
+++ script/main.js
@@ -1,25 +1,53 @@
- var val = [0.0, 1.0];
+//var i = 1;                     //  set your counter to 1
+
+// function myLoop () {           //  create a loop function
+//    setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+//       alert('hello');          //  your code here
+//       i++;                     //  increment the counter
+//       if (i < 10) {            //  if the counter < 10, call the loop function
+//          myLoop();             //  ..  again which will trigger another 
+//       }                        //  ..  setTimeout()
+//    }, 3000)
+// }
  
- for (var i = 1; i <= 5; i++) {
+setInterval(function(){
+var val = [0.0, 1.0];
+ 
+var numbers = [1,2,3,4,5];
+
+ //$.each(numbers, function(i, numbers){
+//setInterval(function(){
+for (var i = 1; i <= 5; i++) {
     $.ajax({
         url: 'https://api.thingspeak.com/channels/693492/field/1/last.txt',
         type: 'GET',
         beforeSend: function() {
-            console.log("Downloading ");
+            //console.log("Downloading ");
         },
         async: false,
         complete: function() {
         },
         success: function(data) {
-            console.log("Download Success ");
+            //console.log("Download Success ");
 			var text = data;
 			val.push(parseFloat(text));
+			
+			//  setTimeout(function(){
+			//  	console.log("this is iteration")
+
+			//  }, 10000);
+
+			//setInterval(function(){ console.log("hello"); }, 10000);
+			console.log("hello");
+
         }
-    });
-}
+	});
+	}
+//});
+//}, 10000);
 
  
-window.onload = function (){ 
+//window.onload = function (){ 
 var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	theme: "light2",
@@ -49,5 +77,6 @@ var chart = new CanvasJS.Chart("chartContainer", {
 });
 chart.render();
 
-}
+//}
 	
+}, 10000);
