0c8d424a5c564e9c28af72be5d3f7c8c1776886e
Prashant <faith.prashant@gmail.com>
2019-03-10 04:02:38 +0530

    37 points and cleaned up code with 30 seconds interval
    Added 38 points of reading in the graph with 30 seconds of interval (seems like it doesn't work when I had put 300 in place of 30000).

--- script/main.js
+++ script/main.js
@@ -1,54 +1,31 @@
-//var i = 1;                     //  set your counter to 1
 
-// function myLoop () {           //  create a loop function
-//    setTimeout(function () {    //  call a 3s setTimeout when the loop is called
-//       alert('hello');          //  your code here
-//       i++;                     //  increment the counter
-//       if (i < 10) {            //  if the counter < 10, call the loop function
-//          myLoop();             //  ..  again which will trigger another 
-//       }                        //  ..  setTimeout()
-//    }, 3000)
-// }
- 
-var val = [0.0, 1.0];
+var val = [];
+
+//Time interval of 30 seconds is set between each reading
+//as Thingsspeak take 30 seconds of interval for each reading
 setInterval(function(){
-//var val = [0.0, 1.0];
- 
-var numbers = [1,2,3,4,5];
+$.ajax({
+	url: 'https://api.thingspeak.com/channels/693492/field/1/last.txt',
+	type: 'GET',
+	beforeSend: function() {
+		//console.log("Downloading ");
+	},
+	async: false,
+	complete: function() {
+	},
+	success: function(data) {
+		//console.log("Download Success ");
+		var text = data;
+		val.push(parseFloat(text));
+		
+		//For debugging on console
+		console.log("Value of temp is" + " " + text);
 
- //$.each(numbers, function(i, numbers){
-//setInterval(function(){
-//for (var i = 1; i <= 5; i++) {
-    $.ajax({
-        url: 'https://api.thingspeak.com/channels/693492/field/1/last.txt',
-        type: 'GET',
-        beforeSend: function() {
-            //console.log("Downloading ");
-        },
-        async: false,
-        complete: function() {
-        },
-        success: function(data) {
-            //console.log("Download Success ");
-			var text = data;
-			val.push(parseFloat(text));
-			
-			//  setTimeout(function(){
-			//  	console.log("this is iteration")
+	}
+});
 
-			//  }, 10000);
 
-			//setInterval(function(){ console.log("hello"); }, 10000);
-			console.log("hello");
-
-        }
-	});
-	//}
-//});	
-//}, 10000);
-
- 
-//window.onload = function (){ 
+//Chart rendering
 var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	theme: "light2",
@@ -72,12 +49,46 @@ var chart = new CanvasJS.Chart("chartContainer", {
 			{ y: val[8] },
 			{ y: val[9] },
 			{ y: val[10] },
-			{ y: val[11] }
+			{ y: val[11] },
+			{ y: val[12] },
+			{ y: val[13] },
+			{ y: val[14] },
+			{ y: val[15] },
+			{ y: val[16] },
+			{ y: val[17] },
+			{ y: val[18] },
+			{ y: val[19] },
+			{ y: val[20] },
+			{ y: val[21] },
+			{ y: val[22] },
+			{ y: val[23] },
+			{ y: val[24] },
+			{ y: val[25] },
+			{ y: val[26] },
+			{ y: val[27] },
+			{ y: val[28] },
+			{ y: val[29] },
+			{ y: val[30] },
+			{ y: val[31] },
+			{ y: val[32] },
+			{ y: val[33] },
+			{ y: val[34] },
+			{ y: val[35] },
+			{ y: val[36] },
+			{ y: val[37] }
 		]
 	}]
 });
 chart.render();
 
-//}
+
+//flushing out the val array to re-regenerate the graph
+if(val.length > 37)
+{
+	for(var j = 0; j < 37; j++)
+		{
+			val.pop()
+		}
+}
 	
-}, 10000);
+}, 30000);
